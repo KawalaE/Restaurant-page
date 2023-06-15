3aa78de234907871b8dbcf8a53146f5df8d0fba5
KawalaE <edyta.wer.kawala@gmail.com>
2023-06-15 21:32:28 +0200

    Add a slider with drinks and change page colors

--- src/home.js
+++ src/home.js
@@ -1,9 +1,10 @@
-/**
- * This file will create homepage as a 
- * function that will be invoked via Event Listiner
- * that is inside index.js file as well as it will be
- * a default page.
- */
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+import Drink1 from './assets/images/blue.png';
+import Drink2 from './assets/images/green.png';
+import Drink3 from './assets/images/pink.png';
+import Drink4 from './assets/images/small-pink.png';
+import Drink5 from './assets/images/small-yellow.png';
 import './style.css';
 let pageContent = document.querySelector('#content');
 
@@ -13,6 +14,31 @@ function createElement(type, text, addClass, parent){
     el.classList.add(addClass);
     parent.appendChild(el);
     return el;
+}
+function initializeSwiper(){
+    const swiper = new Swiper(".swiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 200,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        loop: true,
+        autoplay: {
+            delay: 1,
+            disableOnInteraction: false
+        },
+        speed: 1000,
+        spaceBetween:10,
+      });
 }
 function createNavigatorBar(){
     let universalHeader = createElement('div', '', 'header', pageContent);
@@ -34,6 +60,7 @@ function generateHomeContent(){
     let description = createElement('div', '', 'leftSide', midContent);
     let descriptionTitle = createElement('h2', '', 'desc-title', description);
     let descriptionContent =createElement('div', '', 'desc', description);
+    //let infoButton = createElement('button', 'Learn more', 'button2', description);
     let slider = createElement('div', '', 'slider', midContent);
     descriptionTitle.textContent = "Mixology Manor - Your Spirited Playground!";
     descriptionContent.textContent = ` Step into The Mixology Manor, 
@@ -41,7 +68,41 @@ function generateHomeContent(){
     takes center stage. Our bar is a fusion of creativity, 
     innovation, and a hint of mystery. As you enter, you'll be greeted by skilled mixologists, 
     true artists of the craft, who will take you on a journey of unique flavors 
-    and unforgettable experiences.`
+    and unforgettable experiences.`;
+    
+    
+    const drink1 = new Image();
+    drink1.src = Drink1;
+
+    const drink2 = new Image();
+    drink2.src = Drink2;
+
+    const drink3 = new Image();
+    drink3.src = Drink3;
+
+    const drink4 = new Image();
+    drink4.src = Drink4;
+
+    const drink5 = new Image();
+    drink5.src = Drink5;
+
+    let swiperContainer = createElement('div', '', 'swiper', slider);
+    let swiperWrapper= createElement('div', '', 'swiper-wrapper', swiperContainer);
+    let swiperSlide_1 = createElement('div','', 'swiper-slide', swiperWrapper);
+    swiperSlide_1.appendChild(drink1);
+
+    let swiperSlide_2 = createElement('div','', 'swiper-slide', swiperWrapper);
+    swiperSlide_2.appendChild(drink2);
+
+    let swiperSlide_3 = createElement('div','', 'swiper-slide', swiperWrapper);
+    swiperSlide_3.appendChild(drink3);
+
+    let swiperSlide_4 = createElement('div','', 'swiper-slide', swiperWrapper);
+    swiperSlide_4.appendChild(drink4);
+
+    let swiperSlide_5 = createElement('div','', 'swiper-slide', swiperWrapper);
+    swiperSlide_5.appendChild(drink5);
+    initializeSwiper();
 }
 function initializehomePage(){
     generateHomeBackground();
@@ -49,5 +110,6 @@ function initializehomePage(){
     generateHomeContent();
     
 }
+
 initializehomePage();
 export default createElement;
