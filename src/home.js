a342e0e4873b1e05239e8f62eab402a9cae20288
KawalaE <edyta.wer.kawala@gmail.com>
2023-06-17 13:02:39 +0200

    Create a footer

--- src/home.js
+++ src/home.js
@@ -1,8 +1,18 @@
 import './style.css';
-
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 let content = document.querySelector('#content');
+import Logo from './assets/pictures/logo.png'
+import Icon from './assets/pictures/sushi-icon.png'
+import Footer  from './assets/pictures/github-mark.svg'
 
-function createElement(type, text, addClass, parent){
+const myIcon = new Image();
+myIcon.src = Icon;
+
+const FooterIcon = new Image();
+FooterIcon.src = Footer;
+
+export function createElement(type, text, addClass, parent){
     const el = document.createElement(type);
     el.innerText = text;
     el.classList.add(addClass);
@@ -13,10 +23,52 @@ function createElement(type, text, addClass, parent){
 function initializeBackground(){
     content.classList.add('home-background');
 } 
-function createHomePage(){
+function setPageIcon(icon){
+    let headTitle = document.querySelector('head');
+    let setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel', 'shortcut icon');
+    setFavicon.setAttribute('href', icon);
+    headTitle.appendChild(setFavicon);
+}
+function initializeNavigation(){
+    setPageIcon(myIcon.src);
+    const myLogo = new Image();
+    myLogo.src = Logo;
+    let topBar = createElement('div', '', 'navigation-bar', content);
+    let logo = createElement('div', '', 'logo', topBar);
+    logo.appendChild(myLogo);
+    let navigation = createElement('div', '', 'navigation', topBar);
+    let homeButton = createElement('button','Home', 'navigator-button', navigation);
+    let menuButton = createElement('button', 'Menu', 'navigator-button', navigation);
+    let contactButton = createElement('button', 'Contact', 'navigator-button', navigation);
+
+}
+function initializeHomeContent(){
+    let homeContent = createElement('div', '', 'home-content', content);
+    let homeBox = createElement('div', '', 'home-box', homeContent);
+    let homeInfo = createElement('div', '', 'home-info', homeBox);
+
+    homeInfo.textContent = `Sushi House is a popular sushi 
+    restaurant known for its exquisite Japanese cuisine and 
+    exceptional dining experience. Located in the heart of the 
+    city, Sushi House offers a wide range of traditional and 
+    contemporary sushi creations, prepared with the freshest 
+    ingredients and expert craftsmanship.`
+}
+function initializeFooter(){
+    let footer = createElement('div', '', 'footer',content);
+    let footerContent = createElement('div', 'Created by KawalaE', 'footer-content', footer);
+    let githubMark = document.createElement('a');
+    footer.appendChild(githubMark);
+    githubMark.href = 'https://github.com/KawalaE/Restaurant-page';
+    let githubMarkImg = createElement('img', '', 'github-logo', githubMark);
+    githubMarkImg.src = FooterIcon.src;
+}
+export function createHomePage(){
+    setPageIcon(myIcon.src);
     initializeBackground();
+    initializeNavigation();
+    initializeHomeContent();
+    initializeFooter();
 }
 
-export default createElement;
-
-createHomePage();
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import './home'
 import './style.css'
-import  createElement from './home'
-import Logo from './assets/pictures/logo.png'
+import {createHomePage} from './home'
+
 /**
  * This file should contain tab switching
  */
 
-let content = document.querySelector('#content');
-function initializeNavigation(){
-    const myLogo = new Image();
-    myLogo.src = Logo;
-    let topBar = createElement('div', '', 'navigation-bar', content);
-    let logo = createElement('div', '', 'logo', topBar);
-    logo.appendChild(myLogo);
-    let navigation = createElement('div', '', 'navigation', topBar);
-    let homeButton = createElement('button','Home', 'navigator-button', navigation);
-    let menuButton = createElement('button', 'Menu', 'navigator-button', navigation);
-    let contactButton = createElement('button', 'Contact', 'navigator-button', navigation);
 
-}
-initializeNavigation();
+
+
+
+createHomePage();
